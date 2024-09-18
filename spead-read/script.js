c7c87a2e5581192b45936a3d20abdb1260149249
borovlioff <borovliov.alex@ya.ru>
2024-09-18 20:21:54 +0300

    full screen

--- spead-read/script.js
+++ spead-read/script.js
@@ -149,23 +149,3 @@ endSpeedHTML.addEventListener('input', (e) => {
   localStorage.setItem('endSpeed', endSpeed);
   updateSpeed();
 })
-
-let fullscreen;
-let fsEnter = document.getElementById('full-screen');
-fsEnter.addEventListener('click', function (e) {
-    e.preventDefault();
-    if (!fullscreen) {
-        fullscreen = true;
-        document.documentElement.requestFullscreen();
-        } else {
-        fullscreen = false;
-        document.exitFullscreen();
-    }
-});
-document.addEventListener("fullscreenchange", function() { 
-    if((window.fullScreen) || (window.innerWidth == screen.width && window.innerHeight == screen.height)) {
-        fullscreen = true;
-        } else {
-        fullscreen = false;
-    }
-});  
